3098994a45f722d98cafc0e92af73e11b3aa151f
Benjamin Jelavic <ben.jelavic@beemit.com.au>
2022-02-26 22:54:30 +1100

    fix: project build issues

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-import 'react-native-gesture-handler';
+import "react-native-reanimated";
+import "react-native-gesture-handler";
+
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView } from "react-native";
 import AppLoading from "expo-app-loading";
@@ -34,6 +36,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingBottom: 50
+    paddingBottom: 50,
   },
 });
