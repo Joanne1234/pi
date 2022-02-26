3939033057b2826e00131e275a0ab79d9198a1d0
Benjamin Jelavic <ben.jelavic@beemit.com.au>
2022-02-27 02:47:02 +1100

    feat: get goal details from user input

--- screens/CreateGoalScreen.js
+++ screens/CreateGoalScreen.js
@@ -1,10 +1,24 @@
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import InputCard from "../components/InputCard";
 
 function CreateGoalScreen() {
+  const [title, setTitle] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>Goal Title</Text>
-      <InfoList expanded={true} toDoList={toDoList || []} />
+      <View style={styles.inputWrapper}>
+        <Text style={styles.heading}>Add task to goal</Text>
+        <View style={styles.inputContainer}>
+          <InputCard
+            onChange={(val) => {
+              setTitle(val);
+            }}
+            placeholder="Goal title"
+            value={title}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,12 +28,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F7F7FC",
     alignItems: "center",
+    flexDirection: "column",
   },
   heading: {
     fontFamily: "Poppins_700Bold",
     fontSize: 36,
     lineHeight: 48,
     color: "#14142B",
+  },
+  inputWrapper: {
+    width: "100%",
+    marginTop: 24,
+  },
+  inputContainer: {
+    backgroundColor: "#FCFCFC",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 24,
+    paddingBottom: 24,
+    paddingLeft: 16,
+    paddingRight: 16,
+    margin: 16,
+    borderRadius: 24,
+  },
+  heading: {
+    fontFamily: "Poppins_400Regular",
+    fontSize: 15,
+    lineHeight: 24,
+    color: "#6E7191",
+    marginLeft: 24,
+    marginRight: 24,
+    marginBottom: 0,
   },
 });
 export default CreateGoalScreen;
